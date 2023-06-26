b687eab50c673451d3699b1526c6379409e15942
Xynon <linux@xynon.anonaddy.me>
2023-06-26 01:15:37 -0700

    install hotfix

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import launch
 
-if not launch.is_installed("memory-tempfile"):
+if not launch.is_installed("memory_tempfile"):
     launch.run_pip("install memory-tempfile", "requirements for autoMBW")
-if not launch.is_installed("image-reward"):
+if not launch.is_installed("image_reward"):
     launch.run_pip("install git+https://github.com/Oyaxira/ImageReward.git@main", "requirements for ImageReward")
